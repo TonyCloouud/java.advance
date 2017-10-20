0479436db73df6f6a228d73d0a7afa101d8ee887
白富贵 <fugui.bai@cafintech.com>
2017-10-20 16:10:15 +0800

    add threadPool package

---
+++ src/main/java/advance/concurrent/multThread/ThreadPool.java
@@ -0,0 +1,11 @@
+package advance.concurrent.multThread;
+
+import java.util.concurrent.AbstractExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @autho baifugui
+ * @create 2017 10 20 14:24
+ */
+public class ThreadPool {
+}
